d19d40a26a10e3d89c0b668f7dac9a23f9749e6f
Reinier Boon <rboon@bol.com>
2016-06-09 13:16:22 +0200

    feign client version upgrade
    Change-Id: I7ed3c6cc2b4d1fc8d02c6ddc064766ae804902d1

--- src/main/java/com/bol/feign/callback/JacksonCallback.java
+++ src/main/java/com/bol/feign/callback/JacksonCallback.java
@@ -5,13 +5,13 @@ import com.bol.feign.RequestInterceptorCreator;
 import feign.Feign.Builder;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
-import feign.jaxrs.JAXRSModule;
+import feign.jaxrs.JAXRSContract;
 
 public class JacksonCallback implements BuilderCallback {
     @Override
     public final void configure(Builder builder) {
         //@formatter:off
-        builder.contract(new JAXRSModule.JAXRSContract())
+        builder.contract(new JAXRSContract())
         .decoder(new JacksonDecoder())
         .encoder(new JacksonEncoder())
         .requestInterceptor(RequestInterceptorCreator.json());
--- src/main/java/com/bol/feign/fake/FakeSSLClient.java
+++ src/main/java/com/bol/feign/fake/FakeSSLClient.java
@@ -3,27 +3,13 @@ package com.bol.feign.fake;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSocketFactory;
 
-import dagger.Lazy;
 import feign.Client;
 
 public class FakeSSLClient {
 
     public static Client create() {
-        Lazy<SSLSocketFactory> sslContextFactory = new Lazy<SSLSocketFactory>() {
-            @Override
-            public SSLSocketFactory get() {
-                return new FakeSSLSocketFactory();
-            }
-        };
 
-        Lazy<HostnameVerifier> hostnameVerifier = new Lazy<HostnameVerifier>() {
 
-            @Override
-            public HostnameVerifier get() {
-                return new FakeHostnameVerifier();
-            }
-        };
-
-        return new Client.Default(sslContextFactory, hostnameVerifier);
+        return new Client.Default(new FakeSSLSocketFactory(),  new FakeHostnameVerifier());
     }
 }
